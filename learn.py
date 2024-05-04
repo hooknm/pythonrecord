c80cc227ca921938ec2898bf22949e86742674c8
Gareth Gele <166784821+GarethGele@users.noreply.github.com>
2024-05-03 23:32:12 -0700

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,8 +1,12 @@
 class learn():
 
-  def __init__ (subject, id):
+  def __init__ (self, subject, id):
     self._subject = subject
     self._id = id
 
 def is_id():
+  if self._subject = 'math':
+    return True
+  else:
+    return False
   

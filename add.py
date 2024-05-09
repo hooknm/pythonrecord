275c311d73f43bfaf5960a9ba7c1cd8ee44c89a7
Gareth Gele <166784821+GarethGele@users.noreply.github.com>
2024-05-09 02:34:35 -0700

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,11 @@
 def add(x, y):
-  return x+y
+  return x + y
 result = add(3, 4)
 print(result)
+
+def jian(a, b):
+  return a - b
+result1 = jian(7, 3)
+print(result1)
+end_result = result + result1
+print(end_result)

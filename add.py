2758b652eb6a140c0ec3a9e6259cdf856a2fcdf3
Neo Ming <166784821+hooknm@users.noreply.github.com>
2024-08-06 11:08:46 +0800

    Update add.py

--- add.py
+++ add.py
@@ -9,3 +9,22 @@ result1 = jian(7, 3)
 print(result1)
 end_result = result + result1
 print(end_result)
+
+
+
+"""
+累加器 v1.0
+"""
+
+s = 0
+
+x = int(input('输入整数：'))
+s += x
+
+x = int(input('输入整数：'))
+s += x
+
+x = int(input('输入整数：'))
+s += x
+
+print('总和：%d' % s)

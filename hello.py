08cc9870485213a8246a5f0e201909f0221207f2
Neo Ming <166784821+hooknm@users.noreply.github.com>
2024-08-10 16:16:39 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello,world")
+def complex_function(a, b, c):
+    if not a:
+        return None  # 抛出一个异常可能会更好
+    if not b:
+        return None  # 抛出一个异常可能会更好
+
+    # 一些复杂的代码试着用a,b,c来计算x
+    # 如果成功了，抵制住返回x的诱惑
+    if not x:
+        # 一些关于x的计算的Plan-B
+    return x  # 返回值x只有一个出口点有利于维护代码
